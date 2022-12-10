fe17fca5d18f68294ef8169b6017c7be8abcfce4
Saran <saranbabu2609@gmail.com>
2022-12-10 19:01:28 +0530

    updated

--- index.js
+++ index.js
@@ -1,46 +1,24 @@
 const express=require("express");
-const dotenv=require("dotenv");
+require("dotenv").config()
 const db=require("./db/connect");
 const cors=require("cors")
 const userRouter=require("./router/userRouter")
 const productRouter=require("./router/productRouter")
-
-
-
-
-
-
-dotenv.config()
 const app=express();
 app.use(express.json());
-
-
 // connection
 db();
 app.use(cors())
-
-
-
 //Middleware
 app.use("/api",userRouter);
 app.use("/api",productRouter);
+console.log(process.env.PORT)
+const port= process.env.PORT;
 
 
-
-
-
-const port=process.env.PORT||5000;
-
-
-
-
-
-
+app.use("/products", productRouter);
+app.use("/users", userRouter);
 
 app.listen(port,()=>{
     console.log(`App is Running http://localhost:${port}`);
 });
-app.use("/products", productRouter);
-app.use("/users", userRouter);
-
-app.listen(process.env.PORT);
