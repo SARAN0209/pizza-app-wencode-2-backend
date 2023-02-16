1b0abd95fdeba6284877e0bbe460ff7b9855bbb7
SARAN C <108873756+SARAN0209@users.noreply.github.com>
2023-02-16 17:01:03 +0530

    Add files via upload

--- modules/productModule.js
+++ modules/productModule.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const mongoose=require("mongoose");
 
 
@@ -38,44 +38,5 @@ module.exports=productmodel;
 // 		res.status(500).send(err);
 // 	}
 // };
-=======
-const mongoose=require("mongoose");
 
 
-const productSchema = mongoose.Schema({
-    name:{type: String,required:true},
- 
-    price:{type: Number,required:true},
-  
-    image:{type: String,required:true},
-    desc:{type: String,required:true}
-
-},{
-    timestamps:true,
-})
-const productmodel=  mongoose.model("product",productSchema)
-
-module.exports=productmodel;
-// module.exports.updateProducts = async (req, res) => {
-// 	try {
-// 		const id = req.params.id;
-// 		const updatePostData = await mongo.selectedDb
-// 			.collection("products")
-// 			.findOneAndUpdate({ _id: ObjectId(id) }, { $set: { ...req.body } }, { returnDocument: "after" });
-// 		res.send(updatePostData);
-// 	} catch (err) {
-// 		console.error(err);
-// 		res.status(500).send(err);
-// 	}
-// };
-// module.exports.deleteProducts = async (req, res) => {
-// 	try {
-// 		const id = req.params.id;
-// 		const deletePostData = await mongo.selectedDb.collection("products").remove({ _id: ObjectId(id) });
-// 		res.send(deletePostData);
-// 	} catch (err) {
-// 		console.error(err);
-// 		res.status(500).send(err);
-// 	}
-// };
->>>>>>> a9cc4a0404d88cf9db47fb1890480118199e8210
--- modules/userModule.js
+++ modules/userModule.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const mongoose=require("mongoose");
 
 
@@ -26,33 +26,3 @@ const userSchema= new mongoose.Schema({
 })
 
 const userModel=mongoose.model("user",userSchema);
-=======
-const mongoose=require("mongoose");
-
-
-const userSchema= new mongoose.Schema({
-    name:{
-        type:String,
-        required: true,
-        trim: true
-    },
-    email:{
-        type:String, 
-        unique: true ,
-        required:true,
-        trim:true
-    },
-    password:{
-        type:String,
-        required:true,
-        trim:true
-    },
-	confrimpassword :{
-        type:String,
-        trim:true
-    }
-})
-
-const userModel=mongoose.model("user",userSchema);
->>>>>>> a9cc4a0404d88cf9db47fb1890480118199e8210
-module.exports=userModel
