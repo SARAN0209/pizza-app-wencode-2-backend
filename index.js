2dd22bcc8af4e49f5a155eda7c1c48acb9f1a724
Saran <saranbabu2609@gmail.com>
2023-02-18 17:19:09 +0530

    updates

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express=require("express");
 require("dotenv").config()
 const db=require("./db/connect");
@@ -27,34 +26,4 @@ app.use("/users", userRouter);
 
 app.listen(port,()=>{
     console.log(`App is Running http://localhost:${port}`);
-=======
-const express=require("express");
-require("dotenv").config()
-const db=require("./db/connect");
-const cors=require("cors")
-const userRouter=require("./router/userRouter")
-const productRouter=require("./router/productRouter")
-const app=express();
-app.use(express.json());
-// connection
-db();
-app.use(cors())
-//Middleware
-// app.use("/api",userRouter);
-// app.use("/api",productRouter);
-console.log(process.env.PORT)
-const port= process.env.PORT;
-
-
-app.get('/', (req, res) => {
-    res.send('Welcome to my pizza server!');
-})
-
-app.use("/products", productRouter);
-app.use("/users", userRouter);
-
-
-app.listen(port,()=>{
-    console.log(`App is Running http://localhost:${port}`);
->>>>>>> a9cc4a0404d88cf9db47fb1890480118199e8210
 });
--- router/userRouter.js
+++ router/userRouter.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express=require("express");
 const user=require("../modules/userModule")
 const router=express.Router();
@@ -6,27 +5,46 @@ const jwt =require("jsonwebtoken");
 const bcrypt=require("bcrypt")
 
 
+const checkpassword = (password, confirmpassword) => {
+    return password !== confirmpassword ? false : true;
+  };
+
 router.post("/signup",async(req,res)=>{
     try {
         const exuser= await user.findOne({email:req.body.email});
         if(exuser)
-           return res.json({
+            res.status(409).json({
             message:"You are already exist User Please SignIn Here",
             success:false
 
-           }).status(409);
+           });
+          
+           const isSameePassword = checkpassword(
+            req.body.password,
+            req.body.confirmpassword
+          );
+          if (!isSameePassword) {
+            return res.status(400).send({ msg: "password doesnot match" });
+          } else delete req.body.confirmpassword;
           
           
            const salt=await bcrypt.genSalt(Number(10));
            const hashpassword=await bcrypt.hash(req.body.password,salt)
            
            let newuser = new user({...req.body,password:hashpassword});
-           await newuser.save();
-          return res.send({message:"user create Successfully"}).status(201)
+           await newuser.save((err, data)=>{
+            if (err) {
+                return res.status(400).send({
+                  message: "Error while adding new employee. Please check the data",
+                });
+              }
+              res.send({message:"user create Successfully"}).status(201);
+           });
+        
         
         
     } catch (error) {
-        console.log(err);
+        console.log(error);
         res.send({message:"Internal Server Error"}).status(500)
         
     }
@@ -37,7 +55,7 @@ router.post("/signin",async(req,res)=>{
     try {
         const users=await user.findOne({email:req.body.email})
         if(!users){
-        return res.send({message:"Your not exist user please signup here"}).status(409);
+        return res.send({message:"Your not exist user please signup here"}).status(400);
         }
        
         const validpass=await bcrypt.compare(req.body.password,users.password);
@@ -49,10 +67,10 @@ router.post("/signin",async(req,res)=>{
        
    
        
-        const token=jwt.sign(users.toObject(),process.env.SECRET_KEY,{expiresIn: "1hr"})
+        const token=jwt.sign({users},process.env.SECRET_KEY,{expiresIn: "1hr"})
      
         
-         res.send({token:token}).status(200);
+         res.send(token).status(200);
       
       
         
@@ -86,98 +104,4 @@ router.get("/getuser",(req,res,next)=>{
 
 })
 
-
-
-
-=======
-const express=require("express");
-const user=require("../modules/userModule")
-const router=express.Router();
-const jwt =require("jsonwebtoken");
-const bcrypt=require("bcrypt")
-
-
-router.post("/signup",async(req,res)=>{
-    try {
-        const exuser= await user.findOne({email:req.body.email});
-        if(exuser)
-           return res.json({
-            message:"You are already exist User Please SignIn Here",
-            success:false
-
-           }).status(409);
-          
-          
-           const salt=await bcrypt.genSalt(Number(10));
-           const hashpassword=await bcrypt.hash(req.body.password,salt)
-           
-           await new user({...req.body,password:hashpassword}).save();
-          return res.send({message:"user create Successfully"}).status(201)
-        
-        
-    } catch (error) {
-        console.log(err);
-        res.send({message:"Internal Server Error"}).status(500)
-        
-    }
-    
-})
-router.post("/signin",async(req,res)=>{
-    
-    try {
-        const users=await user.findOne({email:req.body.email})
-        if(!users){
-        return res.send({message:"Your not exist user please signup here"}).status(409);
-        }
-       
-        const validpass=await bcrypt.compare(req.body.password,users.password);
-        // console.log(validpass);
-        if(!validpass){
-        return res.send({message:"Please Enter Valid Password"}).status(409);
-        }
-        
-       
-   
-       
-        const token=jwt.sign(users.toObject(),process.env.SECRET_KEY,{expiresIn: "1hr"})
-     
-        
-         res.send({token:token}).status(200);
-      
-      
-        
-    } catch (error) {
-        res.send({message:`Internal Server Error${error}`}).status(500)
-        
-    }
-
-
-
-
-
-})
-
-
-router.get("/getuser",(req,res,next)=>{
-    user.find((err,data)=>{
-        console.log(data,err);
-        if(err){
-            console.log(err);
-            
-            res.status(404).json({
-                message:"data failed"
-            })
-
-        }
-        else{
-            res.send(data).status(201)
-        }
-    })
-
-})
-
-
-
-
->>>>>>> a9cc4a0404d88cf9db47fb1890480118199e8210
 module.exports=router;
